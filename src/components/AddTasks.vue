73581bd64e046e1337e7a175fa319a3784aff5a1
ujjwal shekdar <ujjwalshekdar@gmail.com>
2023-08-23 18:12:06 +0530

    second commit

---
+++ src/components/AddTasks.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+      <div v-for="task in tasks" :key="task.id">
+        <OnlyTask
+          @toggle-reminder="$emit('toggle-reminder', task.id)"
+          @delete-task="$emit('delete-task', task.id)"
+          :task="task"
+        />
+      </div>
+    </div>
+  </template>
+
+<script>
+import OnlyTask from "./OnlyTask.vue"
+
+export default {
+    name : "AddTasks",
+    props :{
+        tasks: {
+            type: Array,
+            default: () =>[]
+        }
+    },
+    components: {
+        OnlyTask
+    },
+    emits: ['delete-task', 'toggle-reminder']
+}
+</script>
---
+++ volar.config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+	services: [
+		require('volar-service-vetur').default(),
+	],
+	
+};
